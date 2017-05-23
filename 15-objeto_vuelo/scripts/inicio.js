99d1aa08f628d9dbd011dcfdb1e339f75bfa95ca
serlocar <sergio.lopez.cardin@tajamar365.com>
2017-05-23 18:30:01 +0200

    actualizado

--- 15-objeto_vuelo/scripts/inicio.js
+++ 15-objeto_vuelo/scripts/inicio.js
@@ -1,42 +1,60 @@
-let oSample1 = {};
-let oSample2 = new Object();
-
-console.log(typeof oSample1)
-console.log(typeof oSample2)
-
-console.log(oSample1)
-console.log(oSample2)
-
-oSample1.nombre = "";
-oSample2.titulo = "";
-
-oSample2.oLibro = {};
-oSample2.oLibro.titulo = "";
-
-console.log(oSample1)
-console.log(oSample2)
-
-
-let aSample = [];
-let aSample2 = new Array()
-
-
 oFlight = {
-	airline: "Oceanic",
-	number: "815",
-	departure : {
-		claveIATA : "SYD",
-		time : new Date(2004,09,22,14,55),
-		ciudad: "Sydney"
-	},
-	arrival : {
-		claveIATA: "LAX",
-		time: new Date(2004,09,23,10,42),
-		ciudad: "Los Angeles"
-	},
-	mostrar : () => {
-		console.log(oFlight);
-	}
+    airline: "Oceanic",
+    number: "815",
+    departure: {
+        claveIATA: "SYD",
+        time: new Date(2004, 09, 22, 14, 55),
+        ciudad: "Sydney"
+    },
+    arrival: {
+        claveIATA: "LAX",
+        time: new Date(2004, 09, 23, 10, 42),
+        ciudad: "Los Angeles"
+    },
+    mostrar: function() {
+        //console.log(oFlight);
+        for (let key in this) {
+            if (typeof this[key] == "function") {
+                continue
+            } else if (typeof this[key] == "object") {
+                console.log(key.toUpperCase())
+                let obj = this[key]
+                for (let key in obj) {
+                    console.log(
+                        key.toUpperCase() + " : " + obj[key]);
+                }
+            } else {
+                console.log(key.toUpperCase() + " : " + this[key]);
+
+            }
+        }
+    }
 }
 
 oFlight.mostrar()
+
+// let a = "airline"
+// console.log(oFlight[a])
+// console.log(oFlight.airline)
+// console.log(oFlight["airline"])
+
+// let b = "departure"
+// let c = "ciudad"
+// console.log(oFlight[b][c])
+// console.log(oFlight.departure.ciudad)
+// console.log(oFlight["departure"]["ciudad"])
+
+
+oVuelo = {
+    airline: "Oceanic",
+    number: "815",
+    departure: "Sydney",
+    arrival: "Los Angeles",
+    mostrar: function() {
+        for (let key in this) {
+            if (typeof this[key] != "function") {
+                console.log(key.toUpperCase() + " : " + this[key]);
+            }
+        }
+    }
+}
